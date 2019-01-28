c7c0619ab2825845ddbea0406375e23892a2e57d
Priyanka More <priyankamore141194@gmail.com>
2019-01-28 17:36:46 +0530

    Created New class for Transforming Xml file to Html file using XSLT Transforming.

---
+++ JsonRead/src/Jsonreading/XmltoHtml.java
@@ -0,0 +1,55 @@
+package Jsonreading;
+
+
+	 
+	import java.io.File;
+	import java.io.FileWriter;
+	import java.io.IOException;
+	import java.io.StringWriter;
+	 
+	import javax.xml.transform.Source;
+	import javax.xml.transform.Transformer;
+	import javax.xml.transform.TransformerConfigurationException;
+	import javax.xml.transform.TransformerException;
+	import javax.xml.transform.TransformerFactory;
+	import javax.xml.transform.TransformerFactoryConfigurationError;
+	import javax.xml.transform.dom.DOMSource;
+	import javax.xml.transform.stream.StreamResult;
+	import javax.xml.transform.stream.StreamSource;
+	 
+	public class XmltoHtml {
+	 
+		public static void main(String args[]) {
+			Source xml = new StreamSource(new File("F:\\JSon\\product.xml"));
+			Source xslt = new StreamSource("F:\\JSon\\product.xsl");
+	 
+			convertXMLToHTML(xml, xslt);
+	 
+		}
+	 
+		public static void convertXMLToHTML(Source xml, Source xslt) {
+			StringWriter sw = new StringWriter();
+	 
+			try {
+	 
+				FileWriter fw = new FileWriter("F:\\JSon\\product.html");
+				TransformerFactory tFactory = TransformerFactory.newInstance();
+				Transformer trasform = tFactory.newTransformer(xslt);
+				trasform.transform(xml, new StreamResult(sw));
+				fw.write(sw.toString());
+				fw.close();
+	 
+				System.out
+						.println("product.html generated successfully at F:\\JSon ");
+	 
+			} catch (IOException | TransformerConfigurationException e) {
+				e.printStackTrace();
+			} catch (TransformerFactoryConfigurationError e) {
+				e.printStackTrace();
+			} catch (TransformerException e) {
+				e.printStackTrace();
+			}
+	 
+			
+		}
+}
